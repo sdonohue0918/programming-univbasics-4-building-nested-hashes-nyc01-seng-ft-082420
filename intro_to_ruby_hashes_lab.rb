8d3a9e6ace0a835ca64ecc9c53105a8cc0011fc5
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-07-27 12:52:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ end
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
-end
+
 	monopoly = {
 		:railroads=>
 		{
@@ -44,9 +44,6 @@ end
 		}
 		}
 	}
-
-def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 
 end
 
